dc6857d513c8be9d1422f6fc954c4e08b3340e5f
Vovan <vhotsanovich@yandex.ru>
2026-05-10 23:20:53 +0800

    Жалюзи

---
+++ Обои/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Обои
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(41, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Ширина, см";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(41, 64);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Высота, см";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(41, 93);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Материал";
+            // 
+            // label4
+            // 
+            label4.Location = new Point(41, 161);
+            label4.Name = "label4";
+            label4.Size = new Size(240, 51);
+            label4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(41, 125);
+            button1.Name = "button1";
+            button1.Size = new Size(76, 23);
+            button1.TabIndex = 4;
+            button1.Text = "OK";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(123, 33);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(121, 23);
+            textBox1.TabIndex = 5;
+            textBox1.TextChanged += textBox1_TextChanged;
+            textBox1.KeyPress += textBox1_KeyPress;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(123, 61);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(121, 23);
+            textBox2.TabIndex = 6;
+            textBox2.TextChanged += textBox1_TextChanged;
+            textBox2.KeyPress += textBox1_KeyPress;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(123, 90);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 7;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            comboBox1.KeyPress += comboBox1_KeyPress;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(293, 221);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Жалюзи";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private ComboBox comboBox1;
+    }
+}
---
+++ Обои/Form1.cs
@@ -0,0 +1,96 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Обои
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<string, int> BlindsTypes = new Dictionary<string, int>()
+        {
+            {"пластик", 50},
+            {"алюминий", 100},
+            {"бамбук", 75},
+            {"соломка", 70},
+            {"текстиль", 60}
+        };
+        public Dictionary<string, int> BlindsTypesSorted = new Dictionary<string, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            string[] BlindsNames = BlindsTypes.Keys.ToArray();
+            BlindsNames.Sort();
+            foreach (var item in BlindsNames)
+            {
+                this.comboBox1.Items.Add(item);
+                BlindsTypesSorted.Add(item, BlindsTypes[item]);
+            }
+            this.comboBox1.SelectedIndex = 0;
+            this.button1.Enabled = false;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar)) return;
+            if (char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter)
+                {
+                    if (sender.Equals(this.textBox1) && this.textBox1.Text.Length > 0)
+                        this.textBox2.Focus();
+                    else if (sender.Equals(this.textBox2) && this.textBox2.Text.Length > 0)
+                        this.comboBox1.Focus();
+                }
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void comboBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string[] listBlinds = BlindsTypesSorted.Keys.ToArray();
+            if (char.IsLetter(e.KeyChar))
+            {
+                for (int i = 0; i < listBlinds.Length; i++)
+                {
+                    if (listBlinds[i].StartsWith(char.ToLower(e.KeyChar)))
+                        this.comboBox1.SelectedIndex = i;
+                }
+            }
+            if (char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter)
+                {
+                    this.button1.Focus();
+                }
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text != "" && this.textBox2.Text != "")
+                this.button1.Enabled = true;
+            else this.button1.Enabled = false;
+            this.label4.Text = "";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int res;
+            int l = int.TryParse(this.textBox1.Text, out res) ? res : 0;
+            int w = int.TryParse(this.textBox2.Text, out res) ? res : 0;
+            int cost = BlindsTypesSorted[this.comboBox1.Text];
+            decimal price =  l * w / 1000 * cost;
+            this.label4.Text = $"Размеры: высота {l} см, шрина {w} см " +
+                $"\nМатериал: {this.comboBox1.Text}" +
+                $"\nЦена: {price:f2} руб./м.кв.";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.label4.Text = "";
+        }
+    }
+}
